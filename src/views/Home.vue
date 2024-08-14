9d603a2547c37275c5f9085b32f6fcfc0f639dbd
Tane4ka170 <tane4ka170@gmail.com>
2024-08-14 23:40:53 +0300

    added some in Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,26 @@
 <script setup>
 import { computed } from "vue";
 import store from "../store";
+import { RouterLink } from "vue-router";
 
-const meals = computed(() => store.state.meals);
+const letters = "ABCDEFGHIJKLMNPQRSTUVWXYZ".split("");
 </script>
 
 <template>
-  <pre>{{ meals }}</pre>
+  <div class="flex p-8 flex-col">
+    <input
+      type="text"
+      class="rounded border-2 border-slate-200 w-full"
+      placeholder="Search for meals"
+    />
+
+    <div class="flex gap-2 justify-center mt-2">
+      <RouterLink
+        :to="{ name: 'byLetter', params: { letter } }"
+        v-for="letter of letters"
+        :key="letter"
+        >{{ letter }}</RouterLink
+      >
+    </div>
+  </div>
 </template>
