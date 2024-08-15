cb82fa40ded83f3135df9f0b3e0372c8d205fb01
Tane4ka170 <tane4ka170@gmail.com>
2024-08-15 23:48:50 +0300

    started doing header

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,29 @@
 <script setup></script>
 
-<template><router-view /></template>
+<template>
+  <div class="bg-slate-100 h-full">
+    <header class="bg-white shadow h-16 flex justify-between items-stretch">
+      <router-link :to="{ name: 'home' }">Home</router-link>
+      <div class="flex items-center gap-1">
+        <router-link
+          to="/"
+          class="px-2 h-full transition-colors hover:bg-gray-100 inline-flex items-center"
+          >Search Meals</router-link
+        >
+        <router-link
+          to="/"
+          class="px-2 h-full transition-colors hover:bg-gray-100 inline-flex items-center"
+          >Meals by letter</router-link
+        >
+        <router-link
+          to="/"
+          class="px-2 h-full transition-colors hover:bg-gray-100 inline-flex items-center"
+          >Meals by ingredients</router-link
+        >
+      </div>
+    </header>
+    <main><router-view /></main>
+  </div>
+</template>
 
 <style scoped></style>
