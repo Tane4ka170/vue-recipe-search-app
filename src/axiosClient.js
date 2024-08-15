6cd0e1cc4b0305dcd07136572e45222b04a11865
Tane4ka170 <tane4ka170@gmail.com>
2024-08-15 23:45:59 +0300

    renamed

--- src/axiosClient.js
+++ src/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "www.themealdb.com/api/json/v1/1/list.php?i=list",
+  baseURL: "https://www.themealdb.com/api/json/v1/1/list.php?i=list",
 });
 
 export default axiosClient;
