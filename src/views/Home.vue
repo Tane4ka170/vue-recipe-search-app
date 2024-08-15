f9376addd82a23294af352f979f599b471df7a75
Tane4ka170 <tane4ka170@gmail.com>
2024-08-15 23:50:18 +0300

    added some functionality

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,16 @@
 <script setup>
-import { computed } from "vue";
+import { computed, onMounted, ref } from "vue";
 import store from "../store";
 import { RouterLink } from "vue-router";
+import axiosClient from "../axiosClient";
 
 const letters = "ABCDEFGHIJKLMNPQRSTUVWXYZ".split("");
+const ingredients = ref([]);
+
+onMounted(async () => {
+  const response = await axiosClient.get("/list.php?i=list").then();
+  ingredients.value = response.data;
+});
 </script>
 
 <template>
