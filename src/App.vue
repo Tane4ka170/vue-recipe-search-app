b93f0dcf3452e099a65f3dee46581b8cce3b319f
Tane4ka170 <tane4ka170@gmail.com>
2024-08-16 22:33:54 +0300

    Distributed the code to other components

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,7 @@
 <script setup></script>
 
 <template>
-  <div class="bg-slate-100 h-full">
-    <header class="bg-white shadow h-16 flex justify-between items-stretch">
-      <router-link
-        :to="{ name: 'home' }"
-        class="inline-flex items-center h-full px-5"
-        >Home</router-link
-      >
-      <div class="flex items-center gap-1">
-        <router-link
-          to="/"
-          class="px-2 h-full transition-colors hover:bg-gray-100 inline-flex items-center"
-          >Search Meals</router-link
-        >
-        <router-link
-          to="/"
-          class="px-2 h-full transition-colors hover:bg-gray-100 inline-flex items-center"
-          >Meals by letter</router-link
-        >
-        <router-link
-          to="/"
-          class="px-2 h-full transition-colors hover:bg-gray-100 inline-flex items-center"
-          >Meals by ingredients</router-link
-        >
-      </div>
-    </header>
-    <main><router-view /></main>
-  </div>
+  <router-view />
 </template>
 
 <style scoped></style>
