755f5da5dfee449a1a8ad69c934b8922ef704c48
Tane4ka170 <tane4ka170@gmail.com>
2024-08-16 17:36:58 +0300

    Styled router-linkHome

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,11 @@
 <template>
   <div class="bg-slate-100 h-full">
     <header class="bg-white shadow h-16 flex justify-between items-stretch">
-      <router-link :to="{ name: 'home' }">Home</router-link>
+      <router-link
+        :to="{ name: 'home' }"
+        class="inline-flex items-center h-full px-5"
+        >Home</router-link
+      >
       <div class="flex items-center gap-1">
         <router-link
           to="/"
