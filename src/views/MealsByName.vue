10b4ecd4b667b46ffb24fa3bb68b90190dfab244
Tane4ka170 <tane4ka170@gmail.com>
2024-08-16 22:42:04 +0300

    Added search input with basic setup

--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -1,5 +1,19 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+import axiosClient from "../axiosClient";
+
+const keyword = ref("");
+function searchMeals() {}
+</script>
 
 <template>
-  <div>By Name</div>
+  <div class="p-8">
+    <input
+      type="text"
+      v-model="keyword"
+      class="rounded border-2 border-slate-200 w-full"
+      placeholder="Search for meals"
+      @change="searchMeals"
+    />
+  </div>
 </template>
