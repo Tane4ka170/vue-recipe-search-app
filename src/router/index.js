2c745354a5b7c0befc9dd81c5f631232ac026368
Tane4ka170 <tane4ka170@gmail.com>
2024-08-17 17:38:33 +0300

    Update router: added meal routes and layouts.

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import MealsByLetter from "../views/MealsByLetter.vue";
 import MealsByIngredient from "../views/MealsByIngredient.vue";
 import DefaultLayout from "../components/DefaultLayout.vue";
 import GuestLayout from "../components/GuestLayout.vue";
+import MealDetails from "../views/MealDetails.vue";
 
 const routes = [
   {
@@ -22,6 +23,11 @@ const routes = [
         path: "/by-ingredient/:ingredient?",
         name: "byIngredient",
         component: MealsByIngredient,
+      },
+      {
+        path: "/meal/:id",
+        name: "mealDetails",
+        component: MealDetails,
       },
     ],
   },
