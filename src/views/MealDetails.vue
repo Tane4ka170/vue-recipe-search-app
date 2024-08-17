d36ada353730f167283a79ca614110bef59caae2
Tane4ka170 <tane4ka170@gmail.com>
2024-08-18 00:53:37 +0300

    Added YouTubeButton and source links

--- src/views/MealDetails.vue
+++ src/views/MealDetails.vue
@@ -2,6 +2,7 @@
 import { onMounted, ref } from "vue";
 import { useRoute } from "vue-router";
 import axiosClient from "../axiosClient";
+import YouTubeButton from "../components/YouTubeButton.vue";
 
 const route = useRoute();
 const meal = ref({});
@@ -14,16 +15,11 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="w-[800px] mx-auto p-8">
-    <pre>{{ meal }}</pre>
+  <div class="max-w-[800px] mx-auto p-8">
     <h1 v-if="meal.strMeal" class="text-5xl font-bold mb-5">
       {{ meal.strMeal }}
     </h1>
-    <img
-      :src="meal.strMealThumb"
-      :alt="meal.strMeal"
-      class="rounded-t-xl h-48 object-cover w-full"
-    />
+    <img :src="meal.strMealThumb" :alt="meal.strMeal" class="max-w-full" />
     <div class="grid grid-cols-1 sm:grid-cols-3 text-lg py-2">
       <div>
         <strong class="font-bold">Category:</strong> {{ meal.strCategory }}
@@ -34,13 +30,15 @@ onMounted(() => {
       <div><strong class="font-bold">Tags:</strong> {{ meal.strTags }}</div>
     </div>
 
+    <div class="my-3">{{ meal.strInstructions }}</div>
+
     <div class="grid grid-cols-1 sm:grid-cols-2">
       <div>
         <h2 class="text-2xl font-semibold mb-2">Ingredients</h2>
         <ul>
           <template v-for="(el, ind) of new Array(20)" :key="ind">
-            <li v-if="meal[`strMeasure${ind + 1}`]">
-              {{ ind + 1 }}. {{ meal[`strMeasure${ind + 1}`] }}
+            <li v-if="meal[`strIngredient${ind + 1}`]">
+              {{ ind + 1 }}. {{ meal[`strIngredient${ind + 1}`] }}
             </li>
           </template>
         </ul>
@@ -56,6 +54,17 @@ onMounted(() => {
           </template>
         </ul>
       </div>
+    </div>
+
+    <div class="mt-4">
+      <YouTubeButton :href="meal.strYoutube">Go to YouTube</YouTubeButton>
+      <a
+        :href="meal.strSource"
+        target="_blank"
+        class="px-3 py-2 rounded transition-colors text-neutral-700 border-2 border-transparent ml-3"
+      >
+        View original source
+      </a>
     </div>
   </div>
 </template>
