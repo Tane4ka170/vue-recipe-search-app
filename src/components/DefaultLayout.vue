b5e3748b1342c85aeb09dcc89b397f859c549658
Tane4ka170 <tane4ka170@gmail.com>
2024-08-18 00:43:36 +0300

    Simplified component structure

--- src/components/DefaultLayout.vue
+++ src/components/DefaultLayout.vue
@@ -3,8 +3,6 @@ import Navbar from "../components/Navbar.vue";
 </script>
 
 <template>
-  <div class="bg-slate-100 h-full">
-    <navbar />
-    <main><router-view /></main>
-  </div>
+  <navbar />
+  <main><router-view /></main>
 </template>
