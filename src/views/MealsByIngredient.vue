d689a9f59d331daae2ab60deeab0158431f91dcd
Tane4ka170 <tane4ka170@gmail.com>
2024-08-18 00:54:47 +0300

    Connected to Vuex, fetched meals, used Meals

--- src/views/MealsByIngredient.vue
+++ src/views/MealsByIngredient.vue
@@ -1,5 +1,20 @@
-<script setup></script>
+<script setup>
+import { computed, onMounted, ref } from "vue";
+import axiosClient from "../axiosClient";
+import MealItem from "../components/MealItem.vue";
+import { useStore } from "vuex";
+import { useRoute } from "vue-router";
+import Meals from "../components/Meals.vue";
+
+const store = useStore();
+const meals = computed(() => store.state.mealsByIngredient);
+const route = useRoute();
+
+onMounted(() => {
+  store.dispatch("searchMealsByIngredient", route.params.ingredient);
+});
+</script>
 
 <template>
-  <div>By Ingredient</div>
+  <Meals :meals="meals" />
 </template>
