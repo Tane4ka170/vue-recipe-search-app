f2855f19cd578d3bbee9fddff6cd8a3ec32f6ee9
Tane4ka170 <tane4ka170@gmail.com>
2024-08-18 00:47:10 +0300

    Added Link component

---
+++ src/components/YouTubeButton.vue
@@ -0,0 +1,15 @@
+<script setup>
+const props = defineProps({
+  href: String,
+});
+</script>
+
+<template>
+  <a
+    :href="href"
+    target="_blank"
+    class="px-3 py-2 rounded border-2 border-zinc-600 bg-zinc-500 hover:bg-zinc-600 transition-colors text-white"
+  >
+    <slot></slot>
+  </a>
+</template>
