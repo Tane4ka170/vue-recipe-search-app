3f2d91f5f26017198e776444416ada57e6fca38e
Tane4ka170 <tane4ka170@gmail.com>
2024-08-18 00:45:29 +0300

    Implemented meal list component

---
+++ src/components/Meals.vue
@@ -0,0 +1,20 @@
+<script setup>
+import MealItem from "./MealItem.vue";
+
+const { meals } = defineProps({
+  meals: {
+    required: true,
+    type: Object,
+    default: () => [],
+  },
+});
+</script>
+
+<template>
+  <div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-8">
+    <MealItem v-for="meal of meals" :key="meal.idMeal" :meal="meal" />
+  </div>
+  <div v-if="!meals.length" class="flex justify-center text-neutral-600">
+    There are no meals
+  </div>
+</template>
