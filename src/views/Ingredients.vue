98a8a738e7d4400d0db59d443770d00800748a07
Tane4ka170 <tane4ka170@gmail.com>
2024-08-18 17:49:12 +0300

    Added search, navigation, and updated layout

--- src/views/Ingredients.vue
+++ src/views/Ingredients.vue
@@ -1,8 +1,27 @@
 <script setup>
-import { onMounted, ref } from "vue";
+import { computed, onMounted, ref } from "vue";
 import axiosClient from "../axiosClient";
+import { useStore } from "vuex";
+import { useRouter } from "vue-router";
 
+const store = useStore();
+const router = useRouter();
 const ingredients = ref([]);
+const keyword = ref("");
+const computedIngredients = computed(() => {
+  if (!computedIngredients) return ingredients;
+  return ingredients.value.filter((i) => {
+    return i.strIngredient.toLowerCase().includes(keyword.value.toLowerCase());
+  });
+});
+
+function openIngredient(ingredient) {
+  store.commit("setIngredient", ingredient);
+  router.push({
+    name: "byIngredient",
+    params: { ingredient: ingredient.strIngredient },
+  });
+}
 
 onMounted(() => {
   axiosClient.get("list.php?i=list").then(({ data }) => {
@@ -12,21 +31,26 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="p-8">
+  <div class="p-8 pb-0">
     <h1 class="text-4xl font-bold mb-4">Ingredients</h1>
-    <router-link
-      :to="{
-        name: 'byIngredient',
-        params: { ingredient: ingredient.strIngredient },
-      }"
-      v-for="ingredient of ingredients"
-      :key="ingredient.idIngredient"
-      class="block bg-white rounded p-3 mb-3 shadow"
-    >
-      <h3 class="text-2xl font-semibold mb-2">
-        {{ ingredient.strIngredient }}
-      </h3>
-      <p>{{ ingredient.strDescription }}</p>
-    </router-link>
+  </div>
+  <div class="px-8">
+    <input
+      type="text"
+      v-model="keyword"
+      class="rounded border-2 border-slate-200 w-full bg-white mb-3 focus:ring-gray-500"
+      placeholder="Search for ingredients"
+    />
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-3">
+      <a
+        href="#"
+        @click.prevent="openIngredient(ingredient)"
+        v-for="ingredient of computedIngredients"
+        :key="ingredient.idIngredient"
+        class="block bg-white rounded p-3 mb-3 shadow"
+      >
+        <h3 class="text-2xl font-bold mb-2">{{ ingredient.strIngredient }}</h3>
+      </a>
+    </div>
   </div>
 </template>
