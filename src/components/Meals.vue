274a2adb75a799257762f4137c2e4dc0bfb999f1
Tane4ka170 <tane4ka170@gmail.com>
2024-08-18 17:41:05 +0300

    Added padding to Meals component

--- src/components/Meals.vue
+++ src/components/Meals.vue
@@ -11,10 +11,10 @@ const { meals } = defineProps({
 </script>
 
 <template>
-  <div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-8">
+  <div class="grid grid-cols-1 md:grid-cols-3 gap-5 px-8">
     <MealItem v-for="meal of meals" :key="meal.idMeal" :meal="meal" />
   </div>
-  <div v-if="!meals.length" class="flex justify-center text-neutral-600">
+  <div v-if="!meals.length" class="flex justify-center text-neutral-600 p-8">
     There are no meals
   </div>
 </template>
