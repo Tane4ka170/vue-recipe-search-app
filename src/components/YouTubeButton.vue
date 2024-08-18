b82e6b3af9aa5d588e1bda0cb7f4dd61fb6a5dd7
Tane4ka170 <tane4ka170@gmail.com>
2024-08-18 17:42:58 +0300

    Updated YouTube button colors

--- src/components/YouTubeButton.vue
+++ src/components/YouTubeButton.vue
@@ -8,7 +8,7 @@ const props = defineProps({
   <a
     :href="href"
     target="_blank"
-    class="px-3 py-2 rounded border-2 border-zinc-600 bg-zinc-500 hover:bg-zinc-600 transition-colors text-white"
+    class="px-3 py-2 rounded border-2 border-gray-600 bg-gray-500 hover:bg-gray-600 transition-colors text-white"
   >
     <slot></slot>
   </a>
