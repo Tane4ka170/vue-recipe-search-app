46ea6db542042db36b586e7211c51b95c27ae26e
Tane4ka170 <tane4ka170@gmail.com>
2024-08-18 17:50:54 +0300

    Ingredient view: Vuex, router, header

--- src/views/MealsByIngredient.vue
+++ src/views/MealsByIngredient.vue
@@ -1,14 +1,13 @@
 <script setup>
-import { computed, onMounted, ref } from "vue";
-import axiosClient from "../axiosClient";
-import MealItem from "../components/MealItem.vue";
+import { computed, onMounted } from "vue";
 import { useStore } from "vuex";
 import { useRoute } from "vue-router";
 import Meals from "../components/Meals.vue";
 
 const store = useStore();
-const meals = computed(() => store.state.mealsByIngredient);
 const route = useRoute();
+const ingredient = computed(() => store.state.ingredient);
+const meals = computed(() => store.state.mealsByIngredient);
 
 onMounted(() => {
   store.dispatch("searchMealsByIngredient", route.params.ingredient);
@@ -16,5 +15,10 @@ onMounted(() => {
 </script>
 
 <template>
+  <div class="p-8 pb-0">
+    <h1 class="text-4xl font-bold mb-4 text-gray-500">
+      Meals for {{ ingredient.strIngredient }}
+    </h1>
+  </div>
   <Meals :meals="meals" />
 </template>
