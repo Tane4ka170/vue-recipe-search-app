deb5c15d48d99ba8892ae9dec912658994469ae3
Tane4ka170 <tane4ka170@gmail.com>
2024-08-18 17:41:50 +0300

    Improved Navbar link styles

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,23 +2,23 @@
   <header class="bg-white shadow h-16 flex justify-between items-stretch">
     <router-link
       :to="{ name: 'home' }"
-      class="inline-flex items-center h-full px-5"
+      class="inline-flex items-center h-full px-5 text-gray-500 font-bold"
       >Home</router-link
     >
     <div class="flex items-center gap-1">
       <router-link
         :to="{ name: 'byName' }"
-        class="px-2 h-full transition-colors hover:bg-gray-100 inline-flex items-center"
+        class="px-2 h-full transition-colors hover:bg-gray-500 inline-flex items-center hover:text-white"
         >Search Meals</router-link
       >
       <router-link
         :to="{ name: 'byLetter' }"
-        class="px-2 h-full transition-colors hover:bg-gray-100 inline-flex items-center"
+        class="px-2 h-full transition-colors hover:bg-gray-500 inline-flex items-center hover:text-white"
         >Meals by letter</router-link
       >
       <router-link
         :to="{ name: 'ingredients' }"
-        class="px-2 h-full transition-colors hover:bg-gray-100 inline-flex items-center"
+        class="px-2 h-full transition-colors hover:bg-gray-500 inline-flex items-center hover:text-white"
         >Meals by ingredients</router-link
       >
     </div>
