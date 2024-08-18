a55a6619ae73695f4962b71fc6eeaf9dbb0dd49b
Tane4ka170 <tane4ka170@gmail.com>
2024-08-18 17:47:23 +0300

    Displayed random meals

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,22 @@
 <script setup>
 import { computed, onMounted, ref } from "vue";
-import store from "../store";
-import { RouterLink } from "vue-router";
 import axiosClient from "../axiosClient";
+import Meals from "../components/Meals.vue";
 
-const ingredients = ref([]);
+const meals = ref([]);
 
 onMounted(async () => {
-  const response = await axiosClient.get("/list.php?i=list").then();
-  ingredients.value = response.data;
+  for (let i = 0; i < 10; i++) {
+    axiosClient
+      .get(`random.php`)
+      .then(({ data }) => meals.value.push(data.meals[0]));
+  }
 });
 </script>
 
 <template>
-  <div class="flex p-8 flex-col"></div>
+  <div class="p-8 pb-0 text-gray-500">
+    <h1 class="text-4xl font-bold mb-4">Random Meals</h1>
+  </div>
+  <Meals :meals="meals" />
 </template>
